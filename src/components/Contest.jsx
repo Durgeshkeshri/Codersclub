faf849ee7ef72356e2380bbbb69e40cbbbfe6ecc
Durgeshkeshri <84857957+Durgeshkeshri@users.noreply.github.com>
2024-02-03 23:17:43 +0530

    commit

--- src/components/Contest.jsx
+++ src/components/Contest.jsx
@@ -75,28 +75,28 @@ const Contest = () => {
   };
 
   return (
-    <div className="min-h-[645px] p-4  bg-gray-950">
+    <div className="min-h-[645] p-4 sm:p-0 lg:p-4 bg-gray-950">
       <hr className="border-inherit border-slate-800" />
       <div className="flex flex-col md:flex-row justify-center items-center mx-auto mt-7">
         {/* Form Container */}
-        <div className="w-full md:w-2/3 p-8 max-sm:p-2 rounded-md shadow-md mt-10 md:ml-16">
+        <div className="w-full md:w-2/3  p-8 max-sm:p-2 rounded-md shadow-md mt-10 md:ml-16">
           <h2 className="text-[#05D9FF] max-sm:text-center text-5xl custom-text-shadow font-times transition duration-300 max-sm:text-[1.6rem] lg:tracking-wider transform hover:text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 animate-bounce">
             Participate for the upcoming contest
           </h2>
           {/* Form */}
-          <form onSubmit={submitForm} className="space-y-4  max-sm:space-y-1 mt-4">
+          <form onSubmit={submitForm} className="space-y-4 xl:w-[800px] max-sm:space-y-1 mt-4">
             <div>
               <label htmlFor="fullName" className="block text-sm font-medium text-gray-600">
                 Full Name
               </label>
-              <input type="text" id="fullName" ref={fullNameRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required />
+              <input type="text" id="fullName" ref={fullNameRef} className="mt-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required />
             </div>
 
             <div>
               <label htmlFor="course" className="block text-sm font-medium text-gray-600">
                 Course
               </label>
-              <select id="course" ref={courseRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required>
+              <select id="course" ref={courseRef} className="mt-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required>
                 <option value="none">Select your course</option>
                 <option value="BCA">BCA</option>
                 <option value="BBA">BBA</option>
@@ -108,7 +108,7 @@ const Contest = () => {
               <label htmlFor="year" className="block text-sm font-medium text-gray-600">
                 Year
               </label>
-              <select id="year" ref={yearRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required>
+              <select id="year" ref={yearRef} className="mt-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required>
                 <option value="0">Select your year</option>
                 <option value="1">1st Year</option>
                 <option value="2">2nd Year</option>
@@ -121,7 +121,7 @@ const Contest = () => {
               <label htmlFor="div" className="block text-sm font-medium text-gray-600">
                 Div
               </label>
-              <select id="div" ref={divRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required>
+              <select id="div" ref={divRef} className="mt-1 p-2  max-sm:mt-[2px] w-full border rounded-md" required>
                 <option value="Z">Select your div</option>
                 <option value="A">A</option>
                 <option value="B">B</option>
@@ -133,7 +133,7 @@ const Contest = () => {
               <label htmlFor="event" className="block text-sm font-medium text-gray-600">
                 Events
               </label>
-              <select id="event" ref={eventRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required>
+              <select id="event" ref={eventRef} className="mt-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required>
                 <option value="none">Select activites</option>
                 <option value="CodeFusion">CodeFusion</option>
                 <option value="WebWizard">Web Wizard</option>
@@ -145,14 +145,14 @@ const Contest = () => {
               <label htmlFor="gmail" className="block text-sm font-medium text-gray-600">
                 Gmail
               </label>
-              <input type="email" id="gmail" ref={gmailRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required />
+              <input type="email" id="gmail" ref={gmailRef} className="mt-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required />
             </div>
 
             <div>
               <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-600">
                 Phone Number
               </label>
-              <input type="tel" id="phoneNumber" ref={phoneNumberRef} className="mt-1 p-2 max-sm:p-1 max-sm:mt-0 w-full border rounded-md" required />
+              <input type="tel" id="phoneNumber" ref={phoneNumberRef} className="mt-1 max-sm:mb-1 p-2 max-sm:mt-[2px] w-full border rounded-md" required />
             </div>
 
             <button
@@ -166,7 +166,7 @@ const Contest = () => {
         </div>
 
         {/* Image Container */}
-        <div className="hidden md:block md:w-1/3">
+        <div className="hidden lg:block md:w-1/3">
           <img src={bg} alt="" className="w-full h-auto custom-effect" />
         </div>
       </div>
